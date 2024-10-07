62cf194d07925675485a37f44871af53d9ff6bb6
pecan4 <157906054+pecan4@users.noreply.github.com>
2024-10-07 12:21:34 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,84 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    let fly = 0
+    if (fly) {
+        mySprite.vy = -250
+    } else {
+        if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+            mySprite.vy = -250
+        }
+    }
+})
+let mySprite: Sprite = null
+tiles.setCurrentTilemap(tilemap`level2`)
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+characterAnimations.loopFrames(
+mySprite,
+[img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `],
+100,
+characterAnimations.rule(Predicate.MovingRight)
+)
+characterAnimations.loopFrames(
+mySprite,
+[img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `],
+100,
+characterAnimations.rule(Predicate.MovingLeft)
+)
+scene.cameraFollowSprite(mySprite)
+controller.moveSprite(mySprite, 100, 0)
+mySprite.ay = 500
+forever(function () {
+    for (let value of tiles.getTilesByType(null)) {
+    	
+    }
+})
